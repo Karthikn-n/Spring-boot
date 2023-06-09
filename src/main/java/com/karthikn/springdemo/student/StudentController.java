da05288df140f159e35db2ec2f08fbde537b6b45
Karthikn-n <karthikn11092001@gmail.com>
2023-06-09 19:39:58 +0530

    First API

---
+++ src/main/java/com/karthikn/springdemo/student/StudentController.java
@@ -0,0 +1,20 @@
+package com.karthikn.springdemo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+    @GetMapping
+    public List<String> findAllStudents(){
+        return List.of(
+                "Karthi",
+                "virat"
+
+        );
+    }
+}
