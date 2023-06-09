9b075a23e598016907e720e63d42f54b80c140ec
Karthikn-n <karthikn11092001@gmail.com>
2023-06-09 19:00:35 +0530

    The home page is created

---
+++ src/main/java/com/karthikn/springdemo/SpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.karthikn.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // this is the main class
+public class SpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoApplication.class, args);
+	}
+
+}
